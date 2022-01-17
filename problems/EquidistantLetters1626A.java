a80ea84feaf7525c3c3a0442c64aacbcc1a859e1
00100000 <53249541+32n@users.noreply.github.com>
2022-01-17 18:11:40 -0500

    Equidistant Letters + Minor Reduction

---
+++ problems/EquidistantLetters1626A.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+public class EquidistantLetters1626A {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+		int t = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < t; i++) {
+			String s = br.readLine();
+			int[] flist = new int[26];
+			for (int j = 0; j < s.length(); j++) {
+				flist[s.charAt(j) - 97]++;
+			}
+			String ans = "";
+			for (int j = 0; j < 26; j++) {
+				char c = (char)(j + 97);
+				if (flist[j] == 1) {
+					ans = c + ans;
+				} else if (flist[j] == 2) {
+					ans = ans + c + c;
+				}
+			}
+			pw.println(ans);
+		}
+
+		br.close();
+		pw.close();
+	}
+}
